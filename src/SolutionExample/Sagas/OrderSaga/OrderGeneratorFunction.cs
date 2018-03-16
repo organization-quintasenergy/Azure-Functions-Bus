a8fb417a6bd7114aeff178f4935948249dd9e6ff
Pablo Castilla <pablo.castilla@quintasenergy.com>
2018-03-16 16:51:47 +0100

    page using nuget

--- src/SolutionExample/Sagas/OrderSaga/OrderGeneratorFunction.cs
+++ src/SolutionExample/Sagas/OrderSaga/OrderGeneratorFunction.cs
@@ -7,30 +7,30 @@ using OrderSaga.Messages;
 
 namespace OrderSaga
 {
-    public static class OrderGeneratorFunction
-    {
-        static bool finished = false;
+    //public static class OrderGeneratorFunction
+    //{
+    //    static bool finished = false;
 
-        [FunctionName("OrderGeneratorFunction")]
-        public static async Task Run([TimerTrigger("*/15 * * * * *")]TimerInfo myTimer, TraceWriter log)
-        {
-            /*log.Info($"C# Timer trigger function executed at: {DateTime.Now}");
+    //    [FunctionName("OrderGeneratorFunction")]
+    //    public static async Task Run([TimerTrigger("*/15 * * * * *")]TimerInfo myTimer, TraceWriter log)
+    //    {
+    //        log.Info($"C# Timer trigger function executed at: {DateTime.Now}");
 
-            if (finished)
-                return;
+    //        if (finished)
+    //            return;
 
-            await SendOnlyBus.SendAsync(new CartItemAdded() { UserName = "pablo", ProductName = "raspberry pi" }, "ordersaga");
+    //        await SendOnlyBus.SendAsync(new CartItemAdded() { UserName = "pablo", ProductName = "raspberry pi" }, "ordersaga");
 
 
-            Random rnd = new Random();
+    //        Random rnd = new Random();
 
-            if (rnd.Next(10) % 5 == 0)
-            {
-                finished = true;
-                await SendOnlyBus.SendAsync(new ProcessOrder() { UserName = "pablo" }, "ordersaga");
+    //        if (rnd.Next(10) % 5 == 0)
+    //        {
+    //            finished = true;
+    //            await SendOnlyBus.SendAsync(new ProcessOrder() { UserName = "pablo" }, "ordersaga");
                                 
-            }*/
+    //        }
         
-        }
-    }
+    //    }
+    //}
 }

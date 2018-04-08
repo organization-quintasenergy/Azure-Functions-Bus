eb55cd784cb03e212943f10b4b61010afc5a5d05
Pablo <pablo.castilla@gmail.com>
2018-04-08 11:37:15 +0200

    Services connect to the queue

--- src/SolutionExample/UI/UIExample/AFBusService.cs
+++ src/SolutionExample/UI/UIExample/AFBusService.cs
@@ -1,6 +1,8 @@
 ﻿using AFBus;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Hosting;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +11,16 @@ using System.Threading.Tasks;
 
 namespace UIExample
 {
-    public class AFBusService : IHostedService
+    public interface IAFBusService : IHostedService
     {
-        public static HandlersContainer container = new HandlersContainer();
+
+    }
+
+    public class AFBusService : IAFBusService
+    {
+        public static HandlersContainer handlerContainer = new HandlersContainer();
+
+        public static string UI_SERVICE_NAME = "uiexample";
 
         IHubContext<Events> hubcontext;
 
@@ -29,7 +38,26 @@ namespace UIExample
 
         public Task StartAsync(CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(SettingsUtil.GetSettings<string>(SETTINGS.AZURE_STORAGE));
+
+            CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+
+            CloudQueue queue = queueClient.GetQueueReference(UI_SERVICE_NAME.ToLower());
+            queue.CreateIfNotExists();
+
+
+            /*while (true)
+            {
+                // Get the next message
+                CloudQueueMessage retrievedMessage = queue.GetMessage();
+
+                await handlerContainer.HandleAsync(retrievedMessage.AsString, null);
+
+                //Process the message in less than 30 seconds, and then delete the message
+                queue.DeleteMessage(retrievedMessage);
+            }*/
+
+            return Task.CompletedTask;
         }
 
         public Task StopAsync(CancellationToken cancellationToken)

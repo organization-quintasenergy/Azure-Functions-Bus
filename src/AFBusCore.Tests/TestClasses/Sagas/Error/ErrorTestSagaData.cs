938417cf594462c3cd848841801fc814e5636e3c
Pablo Castilla <pablo.castilla@quintasenergy.com>
2018-06-25 14:00:16 +0200

    Fixes #36

---
+++ src/AFBusCore.Tests/TestClasses/Sagas/Error/ErrorTestSagaData.cs
@@ -0,0 +1,12 @@
+﻿using AFBus;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AFBus.Tests.TestClasses
+{
+    public class ErrorTestSagaData : SagaData
+    {
+        public int Counter { get; set; }
+    }
+}

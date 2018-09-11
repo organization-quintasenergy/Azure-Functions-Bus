28955c6560f5f047db73f1b1f591efba7231cef5
Pablo Castilla <pablo.castilla@quintasenergy.com>
2018-09-11 10:40:48 +0200

    serializing recursive

---
+++ src/AFBusCore.Tests/JSONSerializer_tests.cs
@@ -0,0 +1,29 @@
+﻿using AFBus;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AFBusCore.Tests
+{
+    [TestClass]
+    public class JSONSerializer_tests
+    {
+        [TestMethod]
+        public void JSONSerializer_SelfReferencing()
+        {
+            var serializer = new JSONSerializer();
+
+            try
+            {
+                throw new Exception("asdf");
+            }
+            catch (Exception ex)
+            {
+                serializer.Serialize(ex);
+            }
+
+            
+        }
+    }
+}
